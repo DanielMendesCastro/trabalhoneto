a17a4c75841c5f3c1dc58d4c5f906f8423a2df6d
Daniel Mendes Castro <danielmendescastro@outlook.com>
2019-11-18 13:34:23 -0300

    Criando database

--- migrations/20190507193329-create-users.js
+++ migrations/20190507193329-create-users.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('usuario', {
+    return queryInterface.createTable('prefeitura', {
       id: {
         allowNull: false,
         autoIncrement: true,
